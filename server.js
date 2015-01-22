c47fd3c0ffbadbad29f5cb4d7ec01e533d72cf04
Noel Alejandro <noel_ska15@hotmail.com>
2015-01-21 23:28:06 -0600

    Removed Swig cache and Added public static directory for assets

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var Hapi = require('hapi');
+var Swig = require('swig');
 
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
 
+Swig.setDefaults({ cache: false });
+
 server.views({
     path: "./views/",
     engines: {
-        html: require('swig')
+        html: Swig
+    },
+    isCached: false
+});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            listing: true
+        }
     }
 });
 
@@ -16,6 +31,7 @@ server.route({
     handler: function (request, reply) {
         reply.view('index', { title: 'My home page' });
     }
+
 });
 
 server.route({
