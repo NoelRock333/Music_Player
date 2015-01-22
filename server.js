699568464436a0442d1752e8b2ea6c01bd4926c7
Noel Alejandro <noel_ska15@hotmail.com>
2015-01-21 22:38:41 -0600

    Integrated Swig engine

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ var Hapi = require('hapi');
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
 
+server.views({
+    path: "./views/",
+    engines: {
+        html: require('swig')
+    }
+});
+
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply.view('index', { title: 'My home page' });
     }
 });
 
