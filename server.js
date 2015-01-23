a7b6177c81c09b28dcd6cc42bdf27c0b85b15ab4
Angelica Barreda B. <anjelyk.b@gmail.com>
2015-01-22 19:18:13 -0600

    Pruebas

--- server.js
+++ server.js
@@ -1,23 +1,49 @@
 var Hapi = require('hapi');
+var Swig = require('swig');
 
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
+
+Swig.setDefaults({ cache: false });
+
+server.views({
+    path: "./views/",
+    engines: {
+        html: Swig
+    },
+    isCached: false
+});
+
+server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+        directory: {
+            path: 'public',
+            listing: true
+        }
+    }
+});
 
 server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        reply.view('index', { title: 'My home page' });
+        /*reply.view('media', { title: 'Media' });*/
     }
+
 });
 
 server.route({
     method: 'GET',
     path: '/{name}',
     handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+        /*reply('Hello, ' + encodeURIComponent(request.params.name) + '!');*/
+        reply.view('media', { title: 'My home page' });
     }
 });
+
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
