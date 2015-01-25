d575f06a189acb6ffb4198e5ed6a6e8162c10580
angieshates <anjelyk.b@gmail.com>
2015-01-24 18:55:49 -0600

    Modificaciones

---
+++ public/js/interacciones2.js
@@ -0,0 +1,16 @@
+	/*var colorSelect=rgb(25.25.23);
+	var fontColor=rgb(105,237,204);*/
+
+$(document).ready(function(){
+	$('#moresong').click(function()
+	{
+		alert('Cancion agregada con exito!');
+	});
+
+	$(".song").click(function()
+	{
+		alert('sirve');
+		/*$(this).css('background-color',rgb(25.25.23));
+		$(this).css('color',fontColor);*/
+	}); 
+});
